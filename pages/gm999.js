c625ddf84cbfdf73d0c23261e7d8e6ebbfea6f3e
Hans Trauwitz <hans.trauwitz@gmail.com>
2023-03-27 15:49:48 -0600

    prueba ruta garantia ranchera

---
+++ pages/gm999.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import DefaultLayout from './../features/Layouts/DefaultLayout/DefaultLayout';
+
+const Gm999 = () => {
+  return (
+    <DefaultLayout>
+      <div>
+        <span>
+          N° de garantia:
+        </span>
+        <span>
+          GM-999
+        </span>
+      </div>
+      <div>
+        garantia validada
+      </div>
+    </DefaultLayout>
+  );
+}
+
+export default Gm999;
