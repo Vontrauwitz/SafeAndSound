0962086f5c6dfadb99d96c274f7205a1f4fc2c8f
Hans Trauwitz <hans.trauwitz@gmail.com>
2023-03-30 19:59:11 -0600

    cambio del ico y agregar enlace temporal a whatsapp

--- features/common/modules/Navigation/navigationConsts.js
+++ features/common/modules/Navigation/navigationConsts.js
@@ -1,7 +1,13 @@
 import { HiPhoneArrowUpRight } from "react-icons/hi2";
+import { AiOutlineWhatsApp } from "react-icons/ai";
 
 export const navigationLinks = [
   { title: 'Productos', link: '/productos' },
   { title: 'Nosotros', link: '/nosotros' },
   { title: '+ 52 (55) 5555 5555', link: 'tel:(55) 55555555', icon: <HiPhoneArrowUpRight /> },
-]
+  {
+    title: 'WhatsApp',
+    link: 'https://api.whatsapp.com/send?phone=15521288107',
+    icon: <AiOutlineWhatsApp />
+  },
+];
